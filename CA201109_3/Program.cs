e3fbc22b0ff923ab3167d9cbbf178220d9ab4695
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-11-09 13:22:56 +0100

    asd

--- CA201109_3/Program.cs
+++ CA201109_3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,26 @@ namespace CA201109_3
 {
     class Program
     {
-        static void Main(string[] args)
+        static SqlConnection conn;
+        static void Main()
         {
+            conn = new SqlConnection(
+                @"Server=(localdb)\MSSQLLocalDB;" +
+                @"AttachDbFileName=|DataDirectory|\Res\teszt2.mdf;" +
+                "Trusted_Connection=True;");
+
+            conn.Open();
+
+            new SqlCommand("INSERT INTO emberek (orszagId, nev) VALUES (1, 'Zsófika');", conn)
+                .ExecuteNonQuery();
+
+            var sql = new SqlCommand("SELECT * FROM emberek;", conn);
+            var r = sql.ExecuteReader();
+            while (r.Read()) Console.WriteLine(r[2]);
+
+            conn.Close();
+            Console.ReadKey();
+
         }
     }
 }
